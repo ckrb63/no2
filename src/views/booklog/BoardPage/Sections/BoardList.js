f2d8c75b853dcd2aa0748ca2fd3fcf99d5f52f29
rohhy@naver.com <Srohhy@naver.com6>
2022-02-03 13:17:46 +0900

    페이지 라우팅 수정

--- src/views/booklog/BoardPage/Sections/BoardList.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function BoardList(){
-    return <div>BoardList</div>;
-}
-
-export default BoardList
--- src/views/booklog/index.js
+++ src/views/booklog/index.js
@@ -1,15 +1,15 @@
-import BoardPage from "./BoardPage/BoardPage";
-import { Routes, Route } from "react-router-dom";
-import ArticlePage from "./ArticlePage/ArticlePage";
-import RegisterPage from "./RegisterPage/RegisterPage";
-function Booklog() {
-    return (
-        <Routes>
-          <Route index path="/" element={<BoardPage />} />
-          <Route path="/article/:articleId" exact element={<ArticlePage />} />
-          <Route path="/register" exact element={<RegisterPage />} />
-        </Routes>
-    );
-  }
+// import BoardPage from "./BoardPage/BoardPage";
+// import { Routes, Route } from "react-router-dom";
+// import ArticlePage from "./ArticlePage/ArticlePage";
+// import RegisterPage from "./RegisterPage/RegisterPage";
+// function Booklog() {
+//     return (
+//         <Routes>
+//           <Route index path="/" element={<BoardPage />} />
+//           <Route path="/article/:articleId" exact element={<ArticlePage />} />
+//           <Route path="/register" exact element={<RegisterPage />} />
+//         </Routes>
+//     );
+//   }
   
-  export default Booklog;
+//   export default Booklog;
